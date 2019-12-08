d878c6e195ee41f730dd6e89fed664e739d34242
Steve Blackwell <stevehb+ticonderoga@gmail.com>
2019-12-08 08:17:48 +0800

    solves day 7 part 2

--- 7/solve_2.py
+++ 7/solve_2.py
@@ -32,7 +32,7 @@ def runProgram(machineStates, srcIdx, destIdx):
             firstVal = getParamVal(codes, instPtr, 1, paramModes)
             secondVal = getParamVal(codes, instPtr, 2, paramModes)
             resultIdx = codes[instPtr + 3]
-            print(f"    [{srcIdx}@{instPtr}] add: first={firstVal}, second={secondVal}, resultIdx={resultIdx}, result={firstVal + secondVal}")
+            # print(f"    [{srcIdx}@{instPtr}] add: first={firstVal}, second={secondVal}, resultIdx={resultIdx}, result={firstVal + secondVal}")
             codes[resultIdx] = firstVal + secondVal
             instPtr = instPtr + 4
 
@@ -41,7 +41,7 @@ def runProgram(machineStates, srcIdx, destIdx):
             firstVal = getParamVal(codes, instPtr, 1, paramModes)
             secondVal = getParamVal(codes, instPtr, 2, paramModes)
             resultIdx = codes[instPtr + 3]
-            print(f"    [{srcIdx}@{instPtr}] mul: first={firstVal}, second={secondVal}, resultIdx={resultIdx}, result={firstVal * secondVal}")
+            # print(f"    [{srcIdx}@{instPtr}] mul: first={firstVal}, second={secondVal}, resultIdx={resultIdx}, result={firstVal * secondVal}")
             codes[resultIdx] = firstVal * secondVal
             instPtr = instPtr + 4
 
@@ -50,17 +50,17 @@ def runProgram(machineStates, srcIdx, destIdx):
             if len(inputVals) > 0:
                 inputVal = inputVals.pop(0)
                 resultIdx = codes[instPtr + 1]
-                print(f"    [{srcIdx}@{instPtr}] input: inputVal={inputVal}, resultIdx={resultIdx}, remaining inputs: {inputVals}")
+                # print(f"    [{srcIdx}@{instPtr}] input: inputVal={inputVal}, resultIdx={resultIdx}, remaining inputs: {inputVals}")
                 codes[resultIdx] = inputVal
                 instPtr = instPtr + 2
             else:
-                print(f"    [{srcIdx}@{instPtr}] input: no inputs; stalling machine {srcIdx}...")
+                # print(f"    [{srcIdx}@{instPtr}] input: no inputs; stalling machine {srcIdx}...")
                 return {"isFinished": False, "lastOutput": lastOutput}
 
         # SET OUTPUT
         elif(opCode == 4):
             outputVal = getParamVal(codes, instPtr, 1, paramModes)
-            print(f"    [{srcIdx}@{instPtr}] OUTPUT: destIdx={destIdx}, value={outputVal}")
+            # print(f"    [{srcIdx}@{instPtr}] OUTPUT: destIdx={destIdx}, value={outputVal}")
             machineStates[destIdx]["inputs"].append(outputVal)
             lastOutput = outputVal
             instPtr = instPtr + 2
@@ -69,7 +69,7 @@ def runProgram(machineStates, srcIdx, destIdx):
         elif(opCode == 5):
             conditionVal = getParamVal(codes, instPtr, 1, paramModes)
             jumpVal = getParamVal(codes, instPtr, 2, paramModes)
-            print(f"    [{srcIdx}@{instPtr}] jmp_if_true: conditionVal={conditionVal}, jumpVal={jumpVal}")
+            # print(f"    [{srcIdx}@{instPtr}] jmp_if_true: conditionVal={conditionVal}, jumpVal={jumpVal}")
             if(conditionVal != 0):
                 instPtr = jumpVal
             else:
@@ -79,7 +79,7 @@ def runProgram(machineStates, srcIdx, destIdx):
         elif(opCode == 6):
             conditionVal = getParamVal(codes, instPtr, 1, paramModes)
             jumpVal = getParamVal(codes, instPtr, 2, paramModes)
-            print(f"    [{srcIdx}@{instPtr}] jmp_if_false: conditionVal={conditionVal}, jumpVal={jumpVal}")
+            # print(f"    [{srcIdx}@{instPtr}] jmp_if_false: conditionVal={conditionVal}, jumpVal={jumpVal}")
             if(conditionVal == 0):
                 instPtr = jumpVal
             else:
@@ -90,7 +90,7 @@ def runProgram(machineStates, srcIdx, destIdx):
             firstVal = getParamVal(codes, instPtr, 1, paramModes)
             secondVal = getParamVal(codes, instPtr, 2, paramModes)
             resultIdx = codes[instPtr + 3]
-            print(f"    [{srcIdx}@{instPtr}] less_than: firstVal={firstVal}, secondVal={secondVal}, resultIdx={resultIdx}")
+            # print(f"    [{srcIdx}@{instPtr}] less_than: firstVal={firstVal}, secondVal={secondVal}, resultIdx={resultIdx}")
             if(firstVal < secondVal):
                 codes[resultIdx] = 1
             else:
@@ -102,7 +102,7 @@ def runProgram(machineStates, srcIdx, destIdx):
             firstVal = getParamVal(codes, instPtr, 1, paramModes)
             secondVal = getParamVal(codes, instPtr, 2, paramModes)
             resultIdx = codes[instPtr + 3]
-            print(f"    [{srcIdx}@{instPtr}] equals: firstVal={firstVal}, secondVal={secondVal}, resultIdx={resultIdx}")
+            # print(f"    [{srcIdx}@{instPtr}] equals: firstVal={firstVal}, secondVal={secondVal}, resultIdx={resultIdx}")
             if(firstVal == secondVal):
                 codes[resultIdx] = 1
             else:
@@ -127,32 +127,28 @@ def runProgram(machineStates, srcIdx, destIdx):
     # print(f"CODES: {codes}")
     return {"isFinished": True, "lastOutput": lastOutput}
 
-codesStr = "3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5"
-permutes = [(5, 6, 7, 8, 9), (6, 7, 8, 9, 5), (9, 8, 7, 6, 5)]
+# codesStr = "3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5"
+# permutes = [(5, 6, 7, 8, 9), (6, 7, 8, 9, 5), (9, 8, 7, 6, 5)]
 
-# codesStr = "3,23,3,24,1002,24,10,24,1002,23,-1,23,101,5,23,23,1,24,23,23,4,23,99,0,0"
+# codesStr = "3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10"
 # permutes = [(0, 1, 2, 3, 4), (4, 3, 2, 1, 0), (5, 4, 3, 2, 1), (1, 0, 4, 3, 2)]
 
-# codesStr = "3,31,3,32,1002,32,10,32,1001,31,-2,31,1007,31,0,33,1002,33,7,33,1,33,31,31,1,32,31,31,4,31,99,0,0,0"
-# permutes = [(0, 1, 2, 3, 4), (4, 3, 2, 1, 0), (5, 4, 3, 2, 1), (1, 0, 4, 3, 2)]
-# permutes = list(itertools.permutations([0, 1, 2, 3, 4]))
-
-# codesStr = "3,8,1001,8,10,8,105,1,0,0,21,38,55,64,81,106,187,268,349,430,99999,3,9,101,2,9,9,1002,9,2,9,101,5,9,9,4,9,99,3,9,102,2,9,9,101,3,9,9,1002,9,4,9,4,9,99,3,9,102,2,9,9,4,9,99,3,9,1002,9,5,9,1001,9,4,9,102,4,9,9,4,9,99,3,9,102,2,9,9,1001,9,5,9,102,3,9,9,1001,9,4,9,102,5,9,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,1,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,101,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,1,9,9,4,9,99,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,99,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,99"
-# permutes = list(itertools.permutations([5, 6, 7, 8, 9]))
+codesStr = "3,8,1001,8,10,8,105,1,0,0,21,38,55,64,81,106,187,268,349,430,99999,3,9,101,2,9,9,1002,9,2,9,101,5,9,9,4,9,99,3,9,102,2,9,9,101,3,9,9,1002,9,4,9,4,9,99,3,9,102,2,9,9,4,9,99,3,9,1002,9,5,9,1001,9,4,9,102,4,9,9,4,9,99,3,9,102,2,9,9,1001,9,5,9,102,3,9,9,1001,9,4,9,102,5,9,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,1,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,101,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,99,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,1,9,9,4,9,99,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,102,2,9,9,4,9,3,9,101,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,102,2,9,9,4,9,99,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,102,2,9,9,4,9,99"
+permutes = list(itertools.permutations([5, 6, 7, 8, 9]))
 
 bestThrust = 0
 bestPhase = (0, 0, 0, 0, 0)
 
 for phases in permutes:
     codes = list(map(int, list(codesStr.split(","))))
-    print(f"Running phases {phases}")
+    # print(f"Running phases {phases}")
 
     machineStates = [
-        {"isFinished": False, "codes": codes, "instPtr": 0, "inputs": [phases[0], 0]},
-        {"isFinished": False, "codes": codes, "instPtr": 0, "inputs": [phases[1]]},
-        {"isFinished": False, "codes": codes, "instPtr": 0, "inputs": [phases[2]]},
-        {"isFinished": False, "codes": codes, "instPtr": 0, "inputs": [phases[3]]},
-        {"isFinished": False, "codes": codes, "instPtr": 0, "inputs": [phases[4]]}
+        {"isFinished": False, "codes": codes.copy(), "instPtr": 0, "inputs": [phases[0], 0]},
+        {"isFinished": False, "codes": codes.copy(), "instPtr": 0, "inputs": [phases[1]]},
+        {"isFinished": False, "codes": codes.copy(), "instPtr": 0, "inputs": [phases[2]]},
+        {"isFinished": False, "codes": codes.copy(), "instPtr": 0, "inputs": [phases[3]]},
+        {"isFinished": False, "codes": codes.copy(), "instPtr": 0, "inputs": [phases[4]]}
     ]
 
     loopCount = 0
@@ -167,17 +163,19 @@ for phases in permutes:
             result = runProgram(machineStates, i, nextIdx)
             isFinished = result["isFinished"] is True
             machineStates[i]["isFinished"] = isFinished
-            print(f"    MAIN[{loopCount}]: machine {i} isFinished={isFinished}: " + ("FINISHED" if isFinished else "not finished") + f": returned {result}\n")
+            # print(f"    MAIN[{loopCount}]: machine {i} isFinished={isFinished}: " + ("FINISHED" if isFinished else "not finished") + f": returned {result}")
+            # print(f"    [{i}]{machineStates[i]['codes']}")
+            # print(f"    [{nextIdx}]{machineStates[nextIdx]['codes']}\n")
             allFinished = allFinished and isFinished
             if(isFinished and i == 4):
                 lastOutput = result["lastOutput"]
-                print(f"    MAIN[{loopCount}]: machine {i} is finished; loop complete: lastOutput={lastOutput}")
+                # print(f"    MAIN[{loopCount}]: machine {i} is finished; loop complete: lastOutput={lastOutput}")
                 allFinished = True
         isLooping = not allFinished
         loopCount += 1
         if(loopCount > 10): break
 
-    print(f"    lastOutput={lastOutput}")
+    print(f"Phases {phases}: lastOutput={lastOutput}")
     if(lastOutput > bestThrust):
         bestThrust = lastOutput
         bestPhase = phases
