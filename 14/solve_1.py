8959fd0f7fbfba958f59f5406bbd6f9e4ab17dd5
Steve Blackwell <stevehb+ticonderoga@gmail.com>
2019-12-14 20:24:59 +0800

    adds placeholders for Day 14

---
+++ 14/solve_1.py
@@ -0,0 +1,17 @@
+import math
+import sys
+# import cvxpy
+# import numpy as np
+
+fp = open("input.txt", "r")
+lines = fp.readlines()
+print(f"input has {len(lines)} lines")
+
+
+
+# for line in lines:
+#     line = line.strip()
+#     if(len(line) == 0): break
+#     row = list(line)
+#     map.append(row)
+
